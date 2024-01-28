acf4fa8e46631201bd4f82b5e3b368242c29c3cb
Adrian <adrianserranomonteiro2@gmail.com>
2024-01-28 02:45:09 +0100

    + Add effect when scrolling down

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,16 @@
 			offset: function() { return $nav.height(); }
 		});
 
+	/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+	var prevScrollpos = window.pageYOffset;
+	window.onscroll = function() {
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById("nav").style.top = "0";
+	} else {
+		document.getElementById("nav").style.top = "-75px";
+	}
+	prevScrollpos = currentScrollPos;
+	}
+
 })(jQuery);
